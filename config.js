0313821bd8f7982cd9bfc3bcc4456f0e85f2eb64
Muhammad Syaekhoni <68496079+MSyaekhoni@users.noreply.github.com>
2020-12-14 22:35:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+window._config = {
+
+    cognito: {
+
+        userPoolId: 'us-east-1_UPsxVXEWF',        
+
+        userPoolClientId: '3j3tc5g4kjdhmhnj2vkhoti1sm',
+
+        region: 'us-west-1'
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://x8fy1pg440.execute-api.us-east-1.amazonaws.com/FP/final-project' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
+};
